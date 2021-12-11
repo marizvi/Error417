8bb8a522be1e44981111b4fba4050a7690418b7e
marizvi <202051121@iiitvadodara.ac.in>
2021-12-11 13:06:40 +0530

    Navigation Bar added

--- my_memories/lib/main.dart
+++ my_memories/lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:my_memories/screens/home.dart';
 import 'package:my_memories/screens/sign_in.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  Map<int, Color> color = {
+    50: Color.fromRGBO(136, 14, 79, .1),
+    100: Color.fromRGBO(136, 14, 79, .2),
+    200: Color.fromRGBO(136, 14, 79, .3),
+    300: Color.fromRGBO(136, 14, 79, .4),
+    400: Color.fromRGBO(136, 14, 79, .5),
+    500: Color.fromRGBO(136, 14, 79, .6),
+    600: Color.fromRGBO(136, 14, 79, .7),
+    700: Color.fromRGBO(136, 14, 79, .8),
+    800: Color.fromRGBO(136, 14, 79, .9),
+    900: Color.fromRGBO(136, 14, 79, 1),
+  };
   @override
   Widget build(BuildContext context) {
+    MaterialColor colorCustom = MaterialColor(0xFF31639c, color);
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SignIn(),
+      theme:
+          ThemeData(primarySwatch: colorCustom, accentColor: Colors.teal[200]),
+      routes: {
+        '/': (ctx) =>
+            HomePage(), //here it should be signIn() but just for check i have placed homepag
+      },
     );
   }
 }
-
---
+++ my_memories/lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App Name'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // onTap: (index) {
+        //   print(index);
+        // }, //will automatically provide index
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        // currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType
+            .shifting, //adds slight animation to navigation bar
+
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text('Categories.')),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.add_box_rounded),
+              title: Text('Add')),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              title: Text('Favourites')),
+        ],
+      ),
+    );
+  }
+}
